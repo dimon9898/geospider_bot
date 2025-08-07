2904c2974e1a351484e22c8375a0ef8b9ab1ff24
MacDimon <macdimon@MacBook-Air-MacDimon.local>
2025-08-07 23:12:01 +0300

    user main

--- handlers/user.py
+++ handlers/user.py
@@ -14,6 +14,17 @@ async def cmd_start(message: Message):
                                'Я бот-Геоспайдер 🕷️\n',
                                parse_mode='HTML',
                                reply_markup=await kb.user_main_kb())
+    
+@user.callback_query(F.data == 'back_to_user_main')
+async def back_to_user_main(callback: CallbackQuery):
+    await callback.message.delete()
+    await callback.message.answer_photo(photo=logo, 
+                               caption='Добро пожаловать!\n\n'
+                               'Уникальная спутниковая сеть дифференциальных\n<b>(базовых | опорных | референцных)</b> геодезических станций - <b>"ГЕОСПАЙДЕР"!</b>\n\n📢 Работы по расширению сети продолжаются.\n\n'
+                               'Я бот-Геоспайдер 🕷️\n',
+                               parse_mode='HTML',
+                               reply_markup=await kb.user_main_kb())   
+     
 
 @user.callback_query(F.data == 'user_service')
 async def user_service(callback: CallbackQuery):
@@ -25,6 +36,28 @@ async def user_service(callback: CallbackQuery):
 
 @user.callback_query(F.data == 'user_verification')
 async def user_verification(callback: CallbackQuery):
-    await callback.answer()
-    file = FSInputFile('./files/приемники.xlsx')
-    await callback.message.answer_document(file, reply_markup=await kb.user_main_kb(), caption='ПРИЕМНИКИ')    
+    await callback.message.delete()
+    file = FSInputFile('./files/Приемники.xlsx')
+    await callback.message.answer_document(file, reply_markup=await kb.back_to_user_service_main_btn())
+
+
+@user.callback_query(F.data == 'user_tariff')
+async def user_tariff(callback: CallbackQuery):
+    await callback.message.delete()
+    file = FSInputFile('./files/Тарифы.png')
+    await callback.message.answer_document(file, reply_markup=await kb.back_to_user_service_main_btn())
+
+@user.callback_query(F.data == 'user_contacts')
+async def user_contacts(callback: CallbackQuery):
+    await callback.message.delete()
+    await callback.message.answer('<b>🌐 Наши контакты</b>\n\n'
+                                  '<b>👨‍💻 Техподдержка:</b>  <code>s@geospider.ru</code>\n'
+                                  '<b>📩 Для обращений:</b>  <code>m@geospider.ru</code>\n'
+                                  '<b>📞 Тел.:</b> +7 (812) 910-13-39\n'
+                                  '<b>📬 E-mail:</b> <code>mail@geospider.ru</code>\n\n'
+                                  '--------<b>Режим работы</b>--------\n\n'
+                                  '▪︎ Пн. – Пт.: 9:00–18:00 \n'
+                                  '▪︎ Сб, Вс.: выходные дни',
+                                  reply_markup=await kb.back_to_user_main_btn(),
+                                  parse_mode='HTML')    
+            
--- keyboards/user_kb.py
+++ keyboards/user_kb.py
@@ -5,7 +5,8 @@ async def user_main_kb():
     kb = InlineKeyboardBuilder()
     
     kb.button(text='📡 Сервисы', callback_data='user_service')
-    return kb.as_markup()
+    kb.button(text='🌐 Наши контакты', callback_data='user_contacts')
+    return kb.adjust(1).as_markup()
 
 async def user_service_main_kb():
     kb = InlineKeyboardBuilder()
@@ -15,3 +16,13 @@ async def user_service_main_kb():
     kb.button(text='📔 Поверки', callback_data='user_verification')
     kb.button(text='⬅️ назад', callback_data='back_to_user_main')
     return kb.adjust(1, 2).as_markup()
+
+async def back_to_user_main_btn():
+    kb = InlineKeyboardBuilder()
+    kb.button(text='назад', callback_data='back_to_user_main')
+    return kb.as_markup()
+
+async def back_to_user_service_main_btn():
+    kb = InlineKeyboardBuilder()
+    kb.button(text='назад', callback_data='user_service')
+    return kb.as_markup()
